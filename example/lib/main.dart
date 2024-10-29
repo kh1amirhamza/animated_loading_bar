7b15ce118987e033de9345d2f1b647c05cb8b163
kh1amirhamza <kh1amirhamza@gmail.com>
2024-10-29 17:18:48 +0600

    🎗️Adding Circular Loading Facility

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,51 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  // @override
+  // Widget build(BuildContext context) {
+  //   final colors = [
+  //     Colors.red,
+  //     Colors.green,
+  //     Colors.blue,
+  //     Colors.red, // Duplicate the first color to ensure seamless transition
+  //   ];
+  //
+  //   final stops = [
+  //     for (var i = 0; i < colors.length; i++) i / (colors.length - 1)
+  //   ];
+  //
+  //   final gradient = SweepGradient(
+  //     startAngle: 0.0,
+  //     endAngle: 2 * 3.14,
+  //     colors: colors,
+  //     stops: stops,
+  //     tileMode: TileMode.clamp,
+  //   );
+  //
+  //   return MaterialApp(
+  //     home: Scaffold(
+  //       appBar: AppBar(title: const Text('Smooth SweepGradient Example')),
+  //       body: const Center(
+  //           child: GradientCircularProgressIndicator(
+  //         radius: 100.0,
+  //         strokeWidth: 10.0,
+  //         value: 1.0, // Progress value (0.0 to 1.0)
+  //         gradient: SweepGradient(
+  //           colors: [
+  //             Colors.red,
+  //             Colors.green,
+  //             Colors.blue,
+  //            Colors.red
+  //           ], // Duplicate first color for seamless transition
+  //           stops: [0.0, 0.33,
+  //             0.66,
+  //            1.0
+  //           ],
+  //         ),
+  //       )),
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,12 +61,87 @@ class MyApp extends StatelessWidget {
         ),
         body: const Center(
           child: AnimatedLoadingBar(
-            colors: [Colors.red, Colors.blue, Colors.green, Colors.purple],
+            // colors: [Colors.red, Colors.blue, Colors.green, Colors.purple,],
+            colors: [
+                          Colors.red,
+                          Colors.green,
+                          Colors.yellow,
+                          Colors.blue,
+                         Colors.red
+            ],
             height: 10.0,
             duration: Duration(seconds: 2),
+            loadingType: LoadingType.circular,
           ),
         ),
       ),
     );
+  }
+}
+
+class GradientCircularProgressIndicator extends StatelessWidget {
+  final double radius;
+  final double strokeWidth;
+  final double value;
+  final SweepGradient gradient;
+
+  const GradientCircularProgressIndicator({
+    required this.radius,
+    required this.strokeWidth,
+    required this.value,
+    required this.gradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size(radius * 2, radius * 2),
+      painter: _GradientCircularProgressPainter(
+        radius: radius,
+        strokeWidth: strokeWidth,
+        value: value,
+        gradient: gradient,
+      ),
+    );
+  }
+}
+
+class _GradientCircularProgressPainter extends CustomPainter {
+  final double radius;
+  final double strokeWidth;
+  final double value;
+  final SweepGradient gradient;
+
+  _GradientCircularProgressPainter({
+    required this.radius,
+    required this.strokeWidth,
+    required this.value,
+    required this.gradient,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect =
+        Rect.fromCircle(center: Offset(radius, radius), radius: radius);
+
+    // Draw background circle
+    final backgroundPaint = Paint()
+      ..color = Colors.grey.withOpacity(0.3)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+    canvas.drawCircle(Offset(radius, radius), radius, backgroundPaint);
+
+    // Draw progress circle with gradient
+    final progressPaint = Paint()
+      ..shader = gradient.createShader(rect)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+    final sweepAngle = 2 * 3.14 * value;
+    canvas.drawArc(rect, -3.14 / 2, sweepAngle, false, progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
--- lib/animated_loading_bar.dart
+++ lib/animated_loading_bar.dart
@@ -7,6 +7,7 @@ class AnimatedLoadingBar extends StatefulWidget {
   final double height;
   final List<Color> colors;
   final Duration duration;
+  final LoadingType loadingType;
 
   /// Creates an [AnimatedLoadingBar].
   ///
@@ -19,6 +20,7 @@ class AnimatedLoadingBar extends StatefulWidget {
     this.height = 10.0,
     required this.colors,
     this.duration = const Duration(seconds: 2),
+    this.loadingType = LoadingType.linear,
   }) : super(key: key);
 
   @override
@@ -30,13 +32,17 @@ class _AnimatedLoadingBarState extends State<AnimatedLoadingBar>
   late AnimationController _controller;
   late Animation<Color?> _animation;
 
+  late AnimationController _controllerGradient;
+  late Animation<double> _animationGradient;
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
       duration: widget.duration,
-    )..repeat(reverse: true);
+    );
+     // ..repeat(reverse: false);
 
     _animation = TweenSequence<Color?>(
       widget.colors.asMap().entries.map((entry) {
@@ -49,6 +55,15 @@ class _AnimatedLoadingBarState extends State<AnimatedLoadingBar>
         );
       }).toList(),
     ).animate(_controller);
+
+    // _controllerGradient = AnimationController(
+    //   vsync: this,
+    //   duration: widget.duration,
+    // )..repeat(reverse: true);
+
+    _animationGradient =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _controller.forward();
   }
 
   @override
@@ -62,12 +77,131 @@ class _AnimatedLoadingBarState extends State<AnimatedLoadingBar>
     return AnimatedBuilder(
       animation: _animation,
       builder: (context, child) {
-        return LinearProgressIndicator(
-          backgroundColor: Colors.grey[200],
-          color: _animation.value,
-          minHeight: widget.height,
-        );
+        // Calculate smooth stops for a balanced distribution of colors
+        final stops = [
+          for (var i = 0; i < widget.colors.length; i++) i / (widget.colors.length - 1)
+        ];
+        return widget.loadingType == LoadingType.circular
+            ? GradientCircularProgressIndicator(
+                radius: 100.0,
+                strokeWidth: 10.0,
+                value: _animationGradient.value, // Progress value (0.0 to 1.0)
+                gradient: SweepGradient(
+                  colors: widget.colors, // Duplicate first color for seamless transition
+                  stops: stops,
+                  //stops: [0.0, 0.33, 0.55, 0.66, 1.0],
+                ))
+            : LinearProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                color: _animation.value,
+                minHeight: widget.height,
+              );
       },
     );
   }
 }
+
+// GradientCircularProgressIndicator(
+// radius: 100.0,
+// strokeWidth: 10.0,
+// value: 1.1, // Progress value (0.0 to 1.0)
+// gradient: SweepGradient(
+// colors: widget.colors, // Duplicate first color for seamless transition
+// stops: [0.0, 0.33,
+// 0.66,
+// 1.0
+// ],
+// ));
+
+// AnimatedBuilder(
+// animation: _animation,
+
+/*  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }*/
+
+class GradientCircularProgressIndicator extends StatelessWidget {
+  final double radius;
+  final double strokeWidth;
+  final double value;
+  final SweepGradient gradient;
+
+  const GradientCircularProgressIndicator({
+    required this.radius,
+    required this.strokeWidth,
+    required this.value,
+    required this.gradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size(radius * 2, radius * 2),
+      painter: _GradientCircularProgressPainter(
+        radius: radius,
+        strokeWidth: strokeWidth,
+        value: value,
+        gradient: gradient,
+      ),
+    );
+  }
+}
+
+class _GradientCircularProgressPainter extends CustomPainter {
+  final double radius;
+  final double strokeWidth;
+  final double value;
+  final SweepGradient gradient;
+
+  _GradientCircularProgressPainter({
+    required this.radius,
+    required this.strokeWidth,
+    required this.value,
+    required this.gradient,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect =
+        Rect.fromCircle(center: Offset(radius, radius), radius: radius);
+
+    // Draw background circle
+    final backgroundPaint = Paint()
+      ..color = Colors.grey.withOpacity(0.3)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+    canvas.drawCircle(Offset(radius, radius), radius, backgroundPaint);
+
+    // Draw progress circle with gradient
+    final progressPaint = Paint()
+      ..shader = gradient.createShader(rect)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+    final sweepAngle = 2 * 3.1416 * value;
+    canvas.drawArc(rect, -3.1416 / 2, sweepAngle, false, progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+enum LoadingType { linear, circular }
